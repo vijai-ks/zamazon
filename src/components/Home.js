c554ea5fef97d104fb499e18eb4c30c41bd5c906
vijai-ks <vijai.ksv@gmail.com>
2023-04-15 14:48:31 +0530

    feat: zamazon-home: add projects to the home page
    Signed-off-by: vijai-ks <vijai.ksv@gmail.com>

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,42 @@
 import React from "react";
 import bannerImage from "../assets/bannerImage.jpg";
+import trackpant from "../assets/trackpant.jpg";
+import Product from "./Product";
+
 function Home() {
   return (
     <div className="home">
       <img src={bannerImage} className="home__image" alt="Home Page Banner" />
+      <div className="home__products">
+        <Product
+          id="1"
+          title="Tockey Men Track Pant"
+          price={200}
+          rating={5}
+          image={trackpant}
+        />
+        <Product
+          id="1"
+          title="Tockey Men Track Pant"
+          price={200}
+          rating={5}
+          image={trackpant}
+        />
+        <Product
+          id="1"
+          title="Tockey Men Track Pant"
+          price={200}
+          rating={5}
+          image={trackpant}
+        />
+        <Product
+          id="1"
+          title="Tockey Men Track Pant"
+          price={200}
+          rating={5}
+          image={trackpant}
+        />
+      </div>
     </div>
   );
 }
